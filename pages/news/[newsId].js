26ffaca416c2be3d05b9767d7269ada70d00e0a3
Avinash <avinashkmg1310@gmail.com>
2024-10-26 21:18:51 +0530

    dynamic-routing

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,15 @@
 // our-domain.com/news/news-details
 
+import { useRouter } from "next/router";
+
 const NewsDetailsPage = () => {
+	const router = useRouter();
+	const { newsId } = router.query;
+	console.log(newsId);
+
+	// send a request to the backend API
+	// to fetch the news item with newsId
+
 	return (
 		<div>
 			<h1>The News Details Page</h1>
