9134c2c73a42f346cff0d2db279543f5fcce1402
Avinash <avinashkmg1310@gmail.com>
2024-10-26 21:55:03 +0530

    Linking pages in NextJS

--- pages/aboutus/index.jsx
+++ pages/aboutus/index.jsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
+// our-domain.com/aboutus
 const AboutUs = () => {
+	const details = [
+		{ id: 1, name: "Yash", role: "Senior Developer" },
+		{ id: 2, name: "Vaibhav", role: "Backend Developer" },
+		{ id: 3, name: "Suresh", role: "Frontend Developer" },
+	];
 	return (
 		<div>
 			<h1>About Us Page</h1>
+			<h2>List of Developers</h2>
+			<ul>
+				{details.map((dev) => (
+					<li key={dev.id}>
+						<Link href={`/aboutus/${dev.id}`}>{dev.name}</Link>
+					</li>
+				))}
+			</ul>
 		</div>
 	);
 };
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,8 +1,25 @@
+import Link from "next/link";
+
 // our-domain.com/news
 const NewsPage = () => {
 	return (
 		<div>
 			<h1>The News Page</h1>
+			<ul>
+				<li>
+					<Link href="/news/next-js-react-framework">
+						NextJS - A React Framework
+					</Link>
+				</li>
+				<li>
+					<Link href="/news/greatness-of-next-js">
+						Greatness of NextJS
+					</Link>
+				</li>
+				<li>
+					<Link href="/news/learn-next-js">Learn NextJS</Link>
+				</li>
+			</ul>
 		</div>
 	);
 };
